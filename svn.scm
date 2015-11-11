4497e73a9a044a38d8fd43f88430a9ab51deae51
sph <sph@posteo.eu>
2015-11-11 11:07:26 +0000

    symbol-alist-* got renamed to alist-quoted-*

--- svn.scm
+++ svn.scm
@@ -3,9 +3,9 @@
 (define (svn-cli)
   (let (arguments ((cli-create (ql (result-count word-part ...) #f #t))))
     (and-let*
-      ( (words (false-if-exception (string-join (symbol-alist-ref arguments word-part) " ")))
+      ( (words (false-if-exception (string-join (alist-quoted-ref arguments word-part) " ")))
         (result-count
-          (false-if-exception (string->number (symbol-alist-ref arguments result-count)))))
+          (false-if-exception (string->number (alist-quoted-ref arguments result-count)))))
       (each display-line (svn-multiple result-count words)))))
 
 (svn-cli)
